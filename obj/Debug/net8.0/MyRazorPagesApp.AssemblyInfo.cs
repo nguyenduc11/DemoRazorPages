079cf14bd0495feea819798632b621bd72716b8e
nguyenduc <nguyenduc11@gmail.com>
2024-10-08 11:56:16 +0700

    just commit

--- obj/Debug/net8.0/MyRazorPagesApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyRazorPagesApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyRazorPagesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41bf9a896423df8bc4dec6dc75e93717b30dfb62")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyRazorPagesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyRazorPagesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
